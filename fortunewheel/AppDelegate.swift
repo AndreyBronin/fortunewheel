b294f86e7e32c675c9c6ff75db5bc015c01f552e
Andrey Bronin <andrey.bronin@insolar.io>
2019-01-06 13:51:58 +0300

    Initial Commit

---
+++ fortunewheel/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  fortunewheel
+//
+//  Created by Andrey Bronin on 06/01/2019.
+//  Copyright © 2019 Insolar. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ fortunewheelTests/fortunewheelTests.swift
@@ -0,0 +1,34 @@
+//
+//  fortunewheelTests.swift
+//  fortunewheelTests
+//
+//  Created by Andrey Bronin on 06/01/2019.
+//  Copyright © 2019 Insolar. All rights reserved.
+//
+
+import XCTest
+@testable import fortunewheel
+
+class fortunewheelTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
